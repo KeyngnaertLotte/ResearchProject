0c85e24abe87d6790607306e5ce3767c29f9e86c
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-01-12 15:30:22 +0100

    feat: tailwind added

--- ResearchProject/src/App.vue
+++ ResearchProject/src/App.vue
@@ -3,8 +3,19 @@
 </script>
 
 <template>
-  
+  <div class="puppet">aaaaaaaaaaa</div>
+  <div class="ai">aaaaaaaaaaa</div>
 </template>
 
 <style scoped>
+.puppet{
+  width: 50%;
+  height: 100%;
+  background: white;
+}
+.ai {
+  width: 50%;
+  height: 100%;
+  background: green;
+}
 </style>
