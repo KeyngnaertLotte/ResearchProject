f9988d7d74c391956b5c1a964c816b3a6e805ebb
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-01-25 16:55:47 +0100

    feat: problem flex?

--- ResearchProject/src/views/PuppetView.vue
+++ ResearchProject/src/views/PuppetView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-full flex justify-center items-center ">
+    <div class="w-full h-full ">
       <!-- <MickyMouse class="scale-[0.7]"/> -->
       <Puppet/>
     </div>
