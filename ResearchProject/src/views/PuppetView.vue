a080abc6655823391895c7b6760ecbeaee01daa3
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2024-01-29 12:03:56 +0100

    hotfix: rightarm error

--- ResearchProject/src/views/PuppetView.vue
+++ ResearchProject/src/views/PuppetView.vue
@@ -21,38 +21,36 @@ export default {
 
 const regex = /this\.puppet\('(#\w+)', \[(-?\d+), (-?\d+)\], \[(-?\d+), (-?\d+)\]\);/g;
 let match;
-
-if ((match = regex.exec(cleanedData)) === null){
-  alert("Oops something went wrong try again")
-}
+let matchFound = false;
 
 while ((match = regex.exec(cleanedData)) !== null) {
+  matchFound = true;
+
   const bodyPart = match[1];
   const position = [parseInt(match[2]), parseInt(match[3])];
   const rotation = [parseInt(match[4]), parseInt(match[5])];
 
-  if (bodyPart === "#rightArm"){
-  this.puppet('#rightArm', [position], [rotation]);
+  if (bodyPart === "#rightArm") {
+    this.puppet('#rightArm', [position], [rotation]);
+  } else if (bodyPart === "#leftArm") {
+    this.puppet('#leftArm', [position], [rotation]);
+  } else if (bodyPart === "#leftLeg") {
+    this.puppet('#leftLeg', [position], [rotation]);
+  } else if (bodyPart === "#rightLeg") {
+    this.puppet('#rightLeg', [position], [rotation]);
+  } else if (bodyPart === "#body") {
+    this.puppet('#body', [position], [rotation]);
+  } else if (bodyPart === "#head") {
+    this.puppet('#head', [position], [rotation]);
+  } else {
+    console.log("error");
+  }
 }
-else if (bodyPart === "#leftArm"){
-  this.puppet('#leftArm', [position], [rotation]);
+
+if (!matchFound) {
+  alert("Oops something went wrong try again")
 }
-else if (bodyPart === "#leftLeg"){
-  this.puppet('#leftLeg', [position], [rotation]);
-}
-else if (bodyPart === "#rightLeg"){
-  this.puppet('#rightLeg', [position], [rotation]);
-}
-else if (bodyPart === "#body"){
-  this.puppet('#body', [position], [rotation]);
-}
-else if (bodyPart === "#head"){
-  this.puppet('#head', [position], [rotation]);
-}
-else{
-  console.log("error")
-}
-}
+
 
 
     });
